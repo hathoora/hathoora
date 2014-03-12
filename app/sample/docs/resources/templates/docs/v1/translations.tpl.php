86c69a48e781ab729131b8d420e456fc02252043
Usman <attock@gmail.com>
2014-03-11 23:39:38 -0500

    more transation documentation...

--- app/sample/docs/resources/templates/docs/v1/translations.tpl.php
+++ app/sample/docs/resources/templates/docs/v1/translations.tpl.php
@@ -1,16 +1,65 @@
 <div class="box">
     <h1>Translations</h1>
     <ul class="outline">
-        <!--<li><a href="#configuration">Application Specific Configuration</a></li>-->
+        <li><a href="#configuration">Configuration</a></li>
     </ul>
 
-    <p>This section is not yet documented, click <a href="/admin/translation">here</a> to view some example.</p>
-
-    <!--
-    <a name="environment"></a>
-    <h2>Defining Environment</h2>
     <p>
-        To setup environment for Hathoora PHP Framework installation you can use the following options:
+        Translations in Hathoora PHP Framework are database driven (and cached) which makes it easy to modify content without any code release. In addition translations can also be used for creating static pages quickly.
     </p>
-    -->
+
+    <a name="configuration"></a>
+    <h2>Configuration</h2>
+    <p>
+        Enable configuration in configuration like so
+    </p>
+    <pre>
+        <code class="hljs Ini">
+            # File config.yml
+
+            hathoora:
+                translation:
+                    enabled: 1
+                    cache_service: @cache@
+                    cache_time: 86400
+                    dsn: default
+                    languages: [ en_US, fr_FR ]
+
+                # cache configurations
+                cache:
+                    debug: 1
+                    pools:
+                        common: { driver: 'memcache', servers: [{host: "localhost", port: 11211}]}
+        </code>
+    </pre>
+    <p>
+        <code class="e">hathoora.translation.enabled = 1</code> adds a <code class="inline">translation</code> service.
+        We also passed a cache service so we do not hit database for every fetch. We also specified language we are supporting.
+    </p>
+    <p>
+        You can now use translation service like so:
+    </p>
+    <pre>
+        <code class="hljs php">
+            // get translation for hathoora_hello_world
+            $helloTranslation = $this->getService('translation')->t(
+                'hathoora_hello_world', array('name' => 'World')
+            );
+
+            // get translations for route
+            $routeTranslations = $this->getService('translation')->getRouteTranslations('hathoora_translation_route',
+                array(
+                    'hathoora_route_example_title' => array(
+                        'date' => date('m/d/y H:i:s')
+                    ),
+                    'hathoora_route_example_body' => array(
+                        'link' => 'http://hathoora.org'
+                    )
+                )
+            );
+        </code>
+    </pre>
+    <p>
+        To learn more and see translation in action click <a href="/admin/translation">here</a>.
+    </p>
 </div>
