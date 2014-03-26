33a65316437a0fea2c8c4ef1240d5b1b3c868207
Usman <attock@gmail.com>
2014-03-25 22:54:34 -0500

    trying hathoora assets symlink

--- docroot/index.php
+++ docroot/index.php
@@ -1,10 +1,9 @@
 <?php
-define('HATHOORA_PROFILE_START_TIME', microtime());
 define('HATHOORA_ROOTPATH', realpath(__DIR__ . '/..') .'/');
+
 require_once('../vendor/autoload.php');
 
 // use prod environment by default
-$env = 'prod';
 
 if (isset($_SERVER['HATHOORA_ENV']))
     $env = $_SERVER['HATHOORA_ENV'];
